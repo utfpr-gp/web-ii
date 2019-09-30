a5ceedf30b682a8bf64329ca2c8ff263bb640387
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2019-09-30 19:38:22 -0300

    Objetos

---
+++ 2019-2/30-09-2019--imovel-velho.js
@@ -0,0 +1,24 @@
+class ImovelVelho extends Imovel {
+
+    constructor(endereco, preco, cidade) {
+        super(endereco, preco);
+        this._cidade = cidade;
+    }
+
+    get cidade() {
+        return this._cidade;
+    }
+
+    set cidade(cidade) {
+        this._cidade = cidade;
+    }
+    imprime() {
+        console.log(`Endereco ${this.endereco}
+                    Preço ${this.preco * 0.8}`);
+    }
+
+    toString() {
+        return 'oi';
+    }
+
+}
