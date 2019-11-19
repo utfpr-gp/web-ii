540fbcf1a1f9efbbcdf4667d6d550429fda7dfb1
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2019-11-19 17:55:17 -0300

    Update files

---
+++ 2019-2/debugger.js
@@ -0,0 +1,34 @@
+function onClick() {
+    if (inputsAreEmpty()) {
+        label.textContent = 'Erro: um ou mais campos estão vazios.';
+        return;
+    }
+    updateLabel();
+}
+
+function inputsAreEmpty() {
+    if (getNumber1() === '' || getNumber2() === '') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateLabel() {
+    var addend1 = getNumber1();
+    var addend2 = getNumber2();
+    var sum = addend1 + addend2;
+    label.textContent = addend1 + ' + ' + addend2 + ' = ' + sum;
+}
+
+function getNumber1() {
+    return inputs[0].value;
+}
+
+function getNumber2() {
+    return inputs[1].value;
+}
+var inputs = document.querySelectorAll('input');
+var label = document.querySelector('p');
+var button = document.querySelector('button');
+button.addEventListener('click', onClick);
